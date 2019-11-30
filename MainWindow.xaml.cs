a4913d8a64624142eff25b8c9eeb2f085257a3a2
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2019-12-01 04:27:46 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System.IO;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+using SWF = System.Windows.Forms;
+
+namespace CopyEveLayoutTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string PROFILE_PATH = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), @"CCP\EVE");
+        public ObservableCollection<string> FilesToOverwrite { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FilesToOverwrite = new ObservableCollection<string>();
+            DataContext = this;
+        }
+
+        private string SelectFile()
+        {
+            using (SWF.OpenFileDialog fileDialog = new SWF.OpenFileDialog())
+            {
+                errorLabel.Text = "";
+
+                fileDialog.InitialDirectory = PROFILE_PATH;
+                if (fileDialog.ShowDialog() == SWF.DialogResult.OK)
+                {
+                    return fileDialog.SafeFileName;
+                }
+                else
+                    return null;
+            }
+        }
+
+        private bool FilenameMatches(string filename, string pattern)
+        {
+            Regex r = new Regex(pattern);
+            Match m = r.Match(filename);
+
+            return m.Success;
+        }
+
+        private void SelectProfileDirectory(object sender, RoutedEventArgs e)
+        {
+            using (SWF.FolderBrowserDialog dirDialog = new SWF.FolderBrowserDialog())
+            {
+                dirDialog.SelectedPath = PROFILE_PATH;
+                dirDialog.ShowDialog();
+                profDirText.Text = dirDialog.SelectedPath;
+                PROFILE_PATH = dirDialog.SelectedPath;
+            }
+        }
+
+        private void SelectUserMaster(object sender, RoutedEventArgs e)
+        {
+            string filename = SelectFile();
+            string pattern = "^(core_user_)(\\d+[.]dat)$"; //Represents the pattern "core_user_[one or more integers].dat"
+
+            if (filename != null)
+            {
+                if (FilenameMatches(filename, pattern))
+                    coreUserMaster.Text = filename;
+                else
+                    UpdateStatus("Selected file is not a valid account settings file", System.Windows.Media.Brushes.Red);
+            }
+        }
+
+        private void SelectCharMaster(object sender, RoutedEventArgs e)
+        {
+            string filename = SelectFile();
+            string pattern = "^(core_char_)(\\d+[.]dat)$"; //Represents the pattern "core_char_[one or more integers].dat"
+
+            if (filename != null)
+            {
+                if (FilenameMatches(filename, pattern))
+                    coreCharMaster.Text = filename;
+                else
+                    UpdateStatus("Selected file is not a valid character settings file", System.Windows.Media.Brushes.Red);
+            }
+        }
+
+        private void AddSlave(object sender, RoutedEventArgs e)
+        {
+            string filename = SelectFile();
+
+            if (filename != null)
+            {
+                string pattern = "^(core_(char|user)_)(\\d+[.]dat)$"; // Represents the pattern "core_[user or char]_[one or more integers].dat"
+                if (FilenameMatches(filename, pattern))
+                    FilesToOverwrite.Add(filename);
+                else
+                    UpdateStatus("Selected file is not a valid account or character settings file", System.Windows.Media.Brushes.Red);
+            }
+        }
+
+        private void RemoveSlave(object sender, RoutedEventArgs e)
+        {
+            if (slaveListView.SelectedItem is string filenameToRemove)
+            {
+                FilesToOverwrite.Remove(filenameToRemove);
+            }
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            bool profileDirSelected = profDirText.Text.Length > 0;
+            bool userMasterSelected = coreUserMaster.Text.Length > 0;
+            bool charMasterSelected = coreCharMaster.Text.Length > 0;
+
+            if (profileDirSelected && (userMasterSelected || charMasterSelected))
+                CopyProfileData();
+            else
+                UpdateStatus("Fill out required fields first", System.Windows.Media.Brushes.Orange);
+        }
+
+        private void CopyProfileData()
+        {
+            string newFilename;
+
+            foreach(string oldFilename in slaveListView.Items)
+            {
+                string oldFile = Path.Combine(PROFILE_PATH, oldFilename);
+                if(File.Exists(oldFile))
+                {
+                    if (oldFilename.StartsWith("core_user"))
+                        newFilename = coreUserMaster.Text;
+                    else if (oldFilename.StartsWith("core_char"))
+                        newFilename = coreCharMaster.Text;
+                    else
+                        throw new FileNotFoundException(oldFilename + " is not a valid settings file");
+
+                    string newFile = Path.Combine(PROFILE_PATH, newFilename);
+                    if (File.Exists(newFile))
+                    {
+                        File.Delete(oldFile);
+                        File.Copy(newFile, oldFile);
+                        UpdateStatus("Settings successfully copied!", System.Windows.Media.Brushes.Green);
+                    }
+                    else
+                    {
+                        UpdateStatus("Master file does not exist. Aborting.", System.Windows.Media.Brushes.Red);
+                        break;
+                    }
+                }
+                else
+                {
+                    UpdateStatus("Wrong directory or slave file does not exist. Aborting.", System.Windows.Media.Brushes.Red);
+                    break;
+                }
+            }
+        }
+
+        private void UpdateStatus(string message, System.Windows.Media.SolidColorBrush color)
+        {
+            errorLabel.Foreground = color;
+            errorLabel.Text = message;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Copy Eve Layout Tool
+This tool allows a user to copy their profile settings between different accounts and characters.
+
+## Usage
+- Download the EXE file from the releases page
+- After running the file, you'll see the below window
+![Image1](Images/image1.png)
+- Once you've selected a profile directory, a master core_user and/or master core_char file, and added at least 1 slave, you're good to hit copy!
+![Image2](Images/image2.png)
+
+## FAQ
+- **What if I only want to copy across user or account settings, but not both?**
+  - Account settings are stored in the core_user files, while character settings are stored in the core_char files. You don't need to copy from both at the same time, and the tool will only copy settings from a selected master file. So if you only select a core_user file, only other core_user files will be overwritten, even if you selected one or more core_char slave files.
+- **Will this tool break my EVE profile?**
+  - As with any tool that plays around with any files, regardless of the context, you should always make a backup
+- **Will this tool steal my passwords?**
+  - You're probably reading this on github so you can just take a look at the source code and see for yourself. CELT simply automates the tedious process of copying filenames to copies of your main character's files.
